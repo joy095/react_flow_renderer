78f74190a68e8ea9c36dc1550e840d27a6a51307
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-05-06 22:45:18 +0530

    build

---
+++ build/precache-manifest.16a8f0f1f728560234dbeb252ef7f5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c998b0e6aca9b5dc0c030dcd59a5dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302c06a36b3b0cb64782",
+    "url": "/static/css/main.2792a73f.chunk.css"
+  },
+  {
+    "revision": "4547d0d027c8b33c545e",
+    "url": "/static/js/2.9c035a79.chunk.js"
+  },
+  {
+    "revision": "6d801b029c15388ffbd1a2fab7ae323b",
+    "url": "/static/js/2.9c035a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302c06a36b3b0cb64782",
+    "url": "/static/js/main.7bf04728.chunk.js"
+  },
+  {
+    "revision": "16b05ed1a3b17699db4e",
+    "url": "/static/js/runtime-main.5a2d2003.js"
+  }
+]);
